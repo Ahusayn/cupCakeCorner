55dd503b693623f3f722b59ea1c2f15a90a1148b
Abdul <ahusaynn@gmail.com>
2024-09-30 15:03:32 +0300

    Initial Commit

---
+++ cupCakeCorner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cupCakeCorner
+//
+//  Created by mac on 30/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cupCakeCorner/cupCakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  cupCakeCornerApp.swift
+//  cupCakeCorner
+//
+//  Created by mac on 30/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct cupCakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
